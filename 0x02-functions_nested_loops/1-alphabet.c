db2d33a5a12750ea73e5f6eecd5374719d1f47a6
“Annah <njugunaannah@gmail.com>
2023-02-21 12:34:31 -0800

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * description: a programme prints alphabets in lowercase
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+		return (0);
+}
