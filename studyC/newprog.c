a6549181e27afbb9b3481f679c697b9e728054fb
“Annah <njugunaannah@gmail.com>
2023-02-20 18:49:52 -0800

    study

---
+++ studyC/newprog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: the program prints a simple ouput
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
