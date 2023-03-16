6ea7398f201c54b22600e46eb7ea66f9d3baf024
“Annah <njugunaannah@gmail.com>
2023-03-16 16:06:11 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,37 +2,42 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[]) {
-    char *num1_str, *num2_str;
-    int num1, num2, result, i;
+int main(int argc, char *argv[])
+{
+char *num1_str, *num2_str;
+int num1, num2, result, i;
 
-    if (argc != 3) {
-        printf("Error\n");
-        return 98;
-    }
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (98);
+	}
 
-    num1_str = argv[1];
-    num2_str = argv[2];
+	num1_str = argv[1];
+	num2_str = argv[2];
 
-    for (i = 0; num1_str[i] != '\0'; i++) {
-        if (!isdigit(num1_str[i])) {
-            printf("Error\n");
-            return 98;
-        }
-    }
+	for (i = 0; num1_str[i] != '\0'; i++)
+	{
+		if (!isdigit(num1_str[i]))
+		{
+		printf("Error\n");
+		return (98);
+		}
 
-    for (i = 0; num2_str[i] != '\0'; i++) {
-        if (!isdigit(num2_str[i])) {
-            printf("Error\n");
-            return 98;
-        }
-    }
+	for (i = 0; num2_str[i] != '\0'; i++)
+	{
+		if (!isdigit(num2_str[i]))
+		{
+		printf("Error\n");
+		return (98);
+	}
+	}
 
-    num1 = atoi(num1_str);
-    num2 = atoi(num2_str);
+	num1 = atoi(num1_str);
+	num2 = atoi(num2_str);
 
-    result = num1 * num2;
-    printf("%d\n", result);
+	result = num1 * num2;
+	printf("%d\n", result);
 
-    return 0;
+	return (0);
 }
